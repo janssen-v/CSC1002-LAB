588ee5021462dde1bb554ecfe8d54fb7c041e25a
janssen-v <janssen.vincentius@gmail.com>
2020-02-18 14:42:29 +0700

    final commit from lesson 1

--- Week 3/Lecture 1.py
+++ Week 3/Lecture 1.py
@@ -2,24 +2,32 @@
 
 nums = []
 
-n1 = input('Input the first number: ')
-n2 = input('Input the second number: ')
-n3 = input('Input the third number: ')
+def promptNumber1():
+    n1 = input('Input the first number: ')
+    n1 = int(n1)
+    nums.append(n1)
 
-n1 = int(n1)
-n2 = int(n2)
-n3 = int(n3)
+def promptNumber2():
+    n2 = input('Input the second number: ')
+    n2 = int(n2)
+    nums.append(n2)
 
-nums.append(n1)
-nums.append(n2)
-nums.append(n3)
+def promptNumber3():
+    n3 = input('Input the third number: ')
+    n3 = int(n3)
+    nums.append(n3)
 
-# check if n1 is largest
-if nums[0] >= nums[1] and nums[0] >= nums[2]:
-    print(nums[0])
-# check if n2 is largest
-elif nums[1] >= nums[0] and nums[1] >= nums[2]:
-    print(nums[1])
-# check if n3 is largest
-else: 
-    print(nums[2])
+def findMax():
+    if nums[0] >= nums[1] and nums[0] >= nums[2]:
+        print(nums[0])
+    # check if n2 is largest
+    elif nums[1] >= nums[2]:
+        print(nums[1])
+    # check if n3 is largest
+    else: 
+        print(nums[2])
+
+promptNumber1()
+promptNumber2()
+promptNumber3()
+findMax()
