9a8c5f5aa77bbf0c08a045e48cfb2b8ade8ae879
janssen-v <janssen.vincentius@gmail.com>
2020-02-18 13:59:51 +0700

    removed repetition on duplicate

--- Week 3/Lecture 1.py
+++ Week 3/Lecture 1.py
@@ -7,8 +7,8 @@ n3 = 1
 if n1 >= n2 and n1 >= n3:
     print(n1)
 # check if n2 is largest
-if n2 >= n1 and n2 >= n3:
+elif n2 >= n1 and n2 >= n3:
     print(n2)
 # check if n3 is largest
-if n3 >= n2 and n3 >= n1:
+elif n3 >= n2 and n3 >= n1:
     print(n3)
