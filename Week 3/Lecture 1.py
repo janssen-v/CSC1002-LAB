c9919c33a8da7d2a6677b9a7292c2c59dc5a9b51
janssen-v <janssen.vincentius@gmail.com>
2020-02-18 14:04:53 +0700

    added input function

--- Week 3/Lecture 1.py
+++ Week 3/Lecture 1.py
@@ -1,7 +1,7 @@
 #Finding the largest integer or something
-n1 = 1
-n2 = 1
-n3 = 1
+n1 = input('Input the first number')
+n2 = input('Input the second number')
+n3 = input('Input the third number')
 
 # check if n1 is largest
 if n1 >= n2 and n1 >= n3:
@@ -10,5 +10,5 @@ if n1 >= n2 and n1 >= n3:
 elif n2 >= n1 and n2 >= n3:
     print(n2)
 # check if n3 is largest
-else:
+else: 
     print(n3)
