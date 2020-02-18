eb67055b0d1d9dc489f9fcd99ab74acf45583b56
janssen-v <janssen.vincentius@gmail.com>
2020-02-18 14:10:00 +0700

    fixed str to int fn

--- Week 3/Lecture 1.py
+++ Week 3/Lecture 1.py
@@ -1,7 +1,11 @@
 #Finding the largest integer or something
-n1 = input(int('Input the first number: '))
-n2 = input(int('Input the second number: '))
-n3 = input(int('Input the third number: '))
+n1 = input('Input the first number: ')
+n2 = input('Input the second number: ')
+n3 = input('Input the third number: ')
+
+n1 = int(n1)
+n2 = int(n2)
+n3 = int(n3)
 
 # check if n1 is largest
 if n1 >= n2 and n1 >= n3:
