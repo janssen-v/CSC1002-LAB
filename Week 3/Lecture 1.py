052fa6708714e60aba7c5519323e14b44a4f013b
janssen-v <janssen.vincentius@gmail.com>
2020-02-18 13:59:27 +0700

    initial version

---
+++ Week 3/Lecture 1.py
@@ -0,0 +1,14 @@
+#Finding the largest integer or something
+n1 = 1
+n2 = 1
+n3 = 1
+
+# check if n1 is largest
+if n1 >= n2 and n1 >= n3:
+    print(n1)
+# check if n2 is largest
+if n2 >= n1 and n2 >= n3:
+    print(n2)
+# check if n3 is largest
+if n3 >= n2 and n3 >= n1:
+    print(n3)
