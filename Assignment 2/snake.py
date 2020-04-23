c97b3ae4e55c0f762f2c49d0c443341b6cc2e448
vjsn <44322614+vjsn@users.noreply.github.com>
2020-04-23 18:42:56 +0700

    A2Q1 Prototype 1

--- Assignment 2/snake.py
+++ Assignment 2/snake.py
@@ -1,3 +1,21 @@
 # The Snake Game
 import turtle as t
 
+# Snake unit
+snake = t.Turtle()
+snake.color('black')
+snake.shape('square')
+snake.penup()
+snake.speed(0)
+
+# Snake turns
+def leftTurn():
+    snake.left(45)
+
+def rightTurn():
+    snake.right(45)
+
+# Keybinds
+t.listen()
+t.onkey(leftTurn, 'Left')
+t.onkey(rightTurn, 'Right')
