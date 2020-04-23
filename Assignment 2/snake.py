92d53755f48c4dd9aa1c89cadca0504e48735353
vjsn <44322614+vjsn@users.noreply.github.com>
2020-04-23 21:48:32 +0700

    A2Q1 added turbo

--- Assignment 2/snake.py
+++ Assignment 2/snake.py
@@ -8,14 +8,27 @@ snake.shape('square')
 snake.penup()
 snake.speed(0)
 
+# Refresh Tick
+speed = 1
+
 # Snake turns
 def leftTurn():
-    snake.left(45)
+    snake.left(90)
 
 def rightTurn():
-    snake.right(45)
+    snake.right(90)
+
+def speedUp():
+    global speed
+    speed += 1
 
 # Keybinds
 t.listen()
 t.onkey(leftTurn, 'Left')
 t.onkey(rightTurn, 'Right')
+t.onkey(speedUp, 'Up')
+
+# Collision Detector
+
+while True:
+    snake.forward(speed)
