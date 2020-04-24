023ea6457be7d919b5126226f8b936f6f5290832
vjsn <44322614+vjsn@users.noreply.github.com>
2020-04-25 00:42:58 +0700

    A2Q1 Working tail function

--- Assignment 2/snake.py
+++ Assignment 2/snake.py
@@ -7,15 +7,14 @@ height = 500
 bgcolor = 'white'
 disp.screensize(width, height, bgcolor)
 
-# Objects
-objects = ['snake', 'monster']
-
+# Game Characters
 # Snake unit
 snake = disp.Turtle()
 snake.color('black')
 snake.shape('square')
 snake.penup()
 snake.speed(0)
+lenSnake = 5
 
 # Monster Unit
 monster = disp.Turtle()
@@ -24,6 +23,10 @@ monster.shape('square')
 monster.penup()
 monster.speed(0)
 
+# Food Object
+food = disp.Turtle()
+
+gameChars = [snake, monster]
 
 # Refresh Tick
 
@@ -32,25 +35,49 @@ def update():
     disp.ontimer(update, 1)
 
 # Snake turns
-def leftTurn():
-    snake.left(90)
 
-def rightTurn():
-    snake.right(90)
+def moveUp():
+    for unit in gameChars:
+        unit.setheading(90)
 
+def moveDown():
+    for unit in gameChars:
+        unit.setheading(270)
+
+def moveLeft():
+    for unit in gameChars:
+        unit.setheading(180)
+
+def moveRight():
+    for unit in gameChars:
+        unit.setheading(0)
+    
 # Keybinds
 disp.listen()
-disp.onkey(leftTurn, 'Left')
-disp.onkey(rightTurn, 'Right')
+disp.onkey(moveUp, 'Up')
+disp.onkey(moveDown, 'Down')
+disp.onkey(moveLeft, 'Left')
+disp.onkey(moveRight, 'Right')
 
 # Collision Detector
 
+
 def moveSnake():
     snake.forward(15)
-
+    
+stamp = 0
 def update():
+    global stamp
+    lenSnake = 5
+    correctLength = False
+    snake.stamp()
     moveSnake()
+    if stamp == lenSnake:
+        snake.clearstamps(1)
+    else:
+        stamp += 1
     disp.ontimer(update, 200)
+    
 
 if __name__ == "__main__":
     update()
